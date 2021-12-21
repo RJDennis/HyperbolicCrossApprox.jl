0d7744732b498c3375b354d8a7d9d824f831de90
RJDennis <richard.dennis@glasgow.ac.uk>
2021-12-21 00:10:05 +0000

    Generalize function-types

--- src/hyperbolic_cross_evaluate.jl
+++ src/hyperbolic_cross_evaluate.jl
@@ -1,92 +1,91 @@
-function hyperbolic_cross_evaluate(weights::Array{T,1},node::Array{T,1},multi_index::Array{S,2}) where {T<:AbstractFloat,S<:Integer}
+function hyperbolic_cross_evaluate(weights::Array{T,1},node::Array{R,1},multi_index::Array{S,2}) where {T<:AbstractFloat,R<:Number,S<:Integer}
   
-    n = 2*maximum(multi_index,dims=1).+1
-    d = length(n) # d is the number of dimensions
+  n = 2*maximum(multi_index,dims=1).+1
+  d = length(n) # d is the number of dimensions
+  
+  base_polynomials        = Array{Array{R,2},1}(undef,d)
+  unique_base_polynomials = Array{Array{R,1},2}(undef,size(multi_index))
+   
+  # Construct the base polynomials
     
-    base_polynomials        = Array{Array{T,2},1}(undef,d)
-    unique_base_polynomials = Array{Array{T,1},2}(undef,size(multi_index))
-     
-    # Construct the base polynomials
-      
-    for i = 1:d
-      base_polynomials[i] = chebyshev_polynomial(n[i]-1,node[i])
-    end
-      
-    # Compute the unique polynomial terms from the base polynomials
-      
-    for i = 1:size(multi_index,1)
-      for j = 1:d
-        if multi_index[i,j] == 0
-          unique_base_polynomials[i,j] = [base_polynomials[j][1]]
-        else
-          unique_base_polynomials[i,j] = [base_polynomials[j][2*multi_index[i,j]], base_polynomials[j][2*multi_index[i,j]+1]]
-        end
+  for i = 1:d
+    base_polynomials[i] = chebyshev_polynomial(n[i]-1,node[i])
+  end
+    
+  # Compute the unique polynomial terms from the base polynomials
+    
+  for i = 1:size(multi_index,1)
+    for j = 1:d
+      if multi_index[i,j] == 0
+        unique_base_polynomials[i,j] = [base_polynomials[j][1]]
+      else
+        unique_base_polynomials[i,j] = [base_polynomials[j][2*multi_index[i,j]], base_polynomials[j][2*multi_index[i,j]+1]]
       end
     end
-        
-    # Construct the first row of the interplation matrix
+  end
       
-    polynomials = Array{T,1}(undef,length(weights))
-    l = 1
-    @inbounds for j = 1:size(multi_index,1)
-      new_polynomials = unique_base_polynomials[j,1]
-      for i = 2:d
-        new_polynomials = kron(new_polynomials,unique_base_polynomials[j,i])
-      end
-      m = length(new_polynomials)
-      polynomials[l:l+m-1] = new_polynomials
-      l += m
-    end
-      
-    estimate = zero(T)
-    for i = 1:length(polynomials)
-      estimate += polynomials[i]*weights[i]
-    end
+  # Construct the first row of the interplation matrix
     
-    return estimate
-    
+  polynomials = Array{R,1}(undef,length(weights))
+  l = 1
+  @inbounds for j = 1:size(multi_index,1)
+    new_polynomials = unique_base_polynomials[j,1]
+    for i = 2:d
+      new_polynomials = kron(new_polynomials,unique_base_polynomials[j,i])
+    end
+    m = length(new_polynomials)
+    polynomials[l:l+m-1] = new_polynomials
+    l += m
   end
     
-  function hyperbolic_cross_evaluate(weights::Array{T,1},node::Array{T,1},multi_index::Array{S,2},domain::Union{Array{T,1},Array{T,2}}) where {T<:AbstractFloat,S<:Integer}
-    
-    if size(domain,2) != length(node)
-      error("domain is inconsistent with the number of dimensions")
-    end
-    
-    node = copy(node)
-  
-    d = length(node)
-    for i = 1:d
-      node[i] = normalize_node(node[i],domain[:,i])
-    end
-    
-    estimate = hyperbolic_cross_evaluate(weights,node,multi_index)
-  
-    return estimate
-    
+  estimate = zero(T)
+  for i = 1:length(polynomials)
+    estimate += polynomials[i]*weights[i]
   end
   
-  function hyperbolic_cross_evaluate(weights::Array{T,1},multi_index::Array{S,2}) where {T<:AbstractFloat,S<:Integer}
-    
-    function goo(x::Array{T,1}) where {T <: AbstractFloat}
-    
-      return hyperbolic_cross_evaluate(weights,x,multi_index)
-    
-    end
-    
-    return goo
-    
-  end
-    
-  function hyperbolic_cross_evaluate(weights::Array{T,1},multi_index::Array{S,2},domain::Union{Array{T,1},Array{T,2}}) where {T<:AbstractFloat,S<:Integer}
-    
-    function goo(x::Array{T,1}) where {T <: AbstractFloat}
-    
-      return hyperbolic_cross_evaluate(weights,x,multi_index,domain)
-    
-    end
-    
-    return goo
-    
+  return estimate
+  
+end
+  
+function hyperbolic_cross_evaluate(weights::Array{T,1},node::Array{R,1},multi_index::Array{S,2},domain::Union{Array{T,1},Array{T,2}}) where {T<:AbstractFloat,R<:Number,S<:Integer}
+  
+  if size(domain,2) != length(node)
+    error("domain is inconsistent with the number of dimensions")
   end
   
+  node = copy(node)
+
+  d = length(node)
+  for i = 1:d
+    node[i] = normalize_node(node[i],domain[:,i])
+  end
+  
+  estimate = hyperbolic_cross_evaluate(weights,node,multi_index)
+
+  return estimate
+  
+end
+
+function hyperbolic_cross_evaluate(weights::Array{T,1},multi_index::Array{S,2}) where {T<:AbstractFloat,S<:Integer}
+  
+  function goo(x::Array{R,1}) where {R<:Number}
+  
+    return hyperbolic_cross_evaluate(weights,x,multi_index)
+  
+  end
+  
+  return goo
+  
+end
+  
+function hyperbolic_cross_evaluate(weights::Array{T,1},multi_index::Array{S,2},domain::Union{Array{T,1},Array{T,2}}) where {T<:AbstractFloat,S<:Integer}
+  
+  function goo(x::Array{R,1}) where {R<:Number}
+  
+    return hyperbolic_cross_evaluate(weights,x,multi_index,domain)
+  
+  end
+  
+  return goo
+  
+end
